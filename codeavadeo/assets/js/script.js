33319103747972f512456c88860ecbc5c0a43a61
Muheenudheen Kasan K K <muheenuiux@gmail.com>
2025-09-16 21:10:30 +0530

    Add files via upload

---
+++ codeavadeo/assets/js/script.js
@@ -0,0 +1,5 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+burger.addEventListener('click', ()=>{
+    nav.classList.toggle('nav-active');
+});
